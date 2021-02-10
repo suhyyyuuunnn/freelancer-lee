1e66ea54554c8b8fb510038c7f5c2137bf84dfeb
suhyyyuuunnn <49545423+suhyyyuuunnn@users.noreply.github.com>
2021-02-11 01:54:35 +0900

    first

--- FreelancerSite/settings.py
+++ FreelancerSite/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'FreelancerSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'dbdb.ctpbbzmmnq3x.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432',
-        'NAME': os.environ['dbdb'],
-        'USER': os.environ['postgre_lee'],
-        'PASSWORD': os.environ['dduhyun04261216!'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
